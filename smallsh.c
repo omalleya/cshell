7efb0e81a8cb0d9e2110b12a5ddac1ae05b976db
omalleya <omalleya@oregonstate.edu>
2017-02-28 09:34:58 -0800

    adds working while loop

--- smallsh.c
+++ smallsh.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_COMMAND_LENGTH 40
+
+void runShell();
 
 void runShell() {
 
+	char* command = malloc(sizeof(char)*MAX_COMMAND_LENGTH);
+
 	do {
-		
-	}while();
+		printf(": ");
+		fgets(command, MAX_COMMAND_LENGTH, stdin);
+
+		//test print of command
+		printf("%s", command);
+
+	}while(strcmp(command, "exit\n") != 0);
 }
 
 int main() {
 
-	printf("Hello World!");
 	runShell();	
 	return 0;
 }
