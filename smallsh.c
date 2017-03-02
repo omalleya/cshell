f8059fb6d6ccfa4325c3783c9d32739ea96f628d
omalleya <omalleya@oregonstate.edu>
2017-03-01 18:04:00 -0800

    changes output and input files for background process

--- smallsh.c
+++ smallsh.c
@@ -187,6 +187,26 @@ int executeShell(char** args, char* inputFile, char* outputFile, int background)
 				fcntl(targetFD, F_SETFD, FD_CLOEXEC);
 			}
 
+			if(background == 1 && outputFile==NULL)
+			{
+				targetFD = open("/dev/null", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+				if (targetFD == -1) { perror("target open()"); exit(1); }
+				printf("targetFD == %d\n", targetFD); // Written to terminal
+				result = dup2(targetFD, 1);
+				if (result == -1) { perror("target dup2()"); exit(2); }
+				fcntl(targetFD, F_SETFD, FD_CLOEXEC);
+			}
+
+			if(background == 1 && inputFile==NULL)
+			{
+				sourceFD = open("/dev/null", O_RDONLY);
+				if (sourceFD == -1) { perror("source open()"); exit(1); }
+				printf("sourceFD == %d\n", sourceFD); // Written to terminal
+				result = dup2(sourceFD, 0);
+				if (result == -1) { perror("source dup2()"); exit(2); }
+				fcntl(sourceFD, F_SETFD, FD_CLOEXEC);
+			}
+
 			execvp(args[0], args);
 
 			perror("CHILD: exec failure!\n");
