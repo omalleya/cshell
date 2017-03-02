7b22e455eec8bde38b79c612b640378abde71254
omalleya <omalleya@oregonstate.edu>
2017-03-02 13:56:51 -0800

    testscript kinda working

--- smallsh.c
+++ smallsh.c
@@ -36,14 +36,10 @@ struct sigaction restOfTheTime_act = {0};
 
 void checkBg()
 {
-	printf("line 39\n");
 	int i=0;
-	printf("line 41\n");
 	for(i=0; i<cur; i++)
 	{
-		printf("line 44\n");
 		int childExitStatus=-5;
-		printf("line 46\n");
 		bgpid[i] = waitpid(bgpid[i], &childExitStatus, WNOHANG);
 		//if process is done
 		if(bgpid[i] == -1)
@@ -51,7 +47,6 @@ void checkBg()
 			//pid has already finished and outputted it's exit status
 		}else if(bgpid[i] != 0)
 		{
-			printf("line 54\n");
 			if(WIFEXITED(childExitStatus))
 				printf("background pid %d is done: exit value %d\n", bgpid[i], WEXITSTATUS(childExitStatus));
 			else if (WIFSIGNALED(childExitStatus))
@@ -65,22 +60,18 @@ void checkBg()
 
 void runShell() {
 
-	printf("line 68\n");
 	char* command = malloc(sizeof(char)*MAX_COMMAND_LENGTH);
 	memset(command, '\0', MAX_COMMAND_LENGTH);
 	int exitStatus = -5;
 
-	printf("line 73\n");
 	do {
 		printf(": ");
 		fflush(stdout);
 		fgets(command, MAX_COMMAND_LENGTH, stdin);
 
 		//parses command and then checks command for proper functionality
-		printf("line 80\n");
 		parseCommand(command, &exitStatus);
 		checkBg();
-		printf("line 83\n");
 
 	}while(1);
 }
@@ -97,17 +88,14 @@ void parseCommand(char* command, int* exitStatus)
 	int background=0;
 
 	//need to copy command because strtok edits command string
-	printf("line 100\n");
 	char* temp = malloc(sizeof(char)*strlen(command));
 	strcpy(temp, command);
 
 	token = strtok(temp, s);
 
 	//figure out how many args there are
-	printf("line 107\n");
 	while( token != NULL ) 
 	{
-		printf("line 110\n");
 		if(strcmp(token, "<")==0)
 		{
 			token = strtok(NULL, s);
@@ -135,15 +123,12 @@ void parseCommand(char* command, int* exitStatus)
 			numArgs++;
 		}
 
-		printf("Token: %s\n", token);
 		token = strtok(NULL, s);
 	}
-	printf("line 139\n");
 	free(temp);
 
 	//create args array with size figured out in while loop
 	char **args = (char**) malloc((numArgs)*sizeof(char*));
-	printf("line 144\n");
 
 	token = strtok(command, s);
 	for(i=0; i<numArgs-1; i++)
@@ -153,17 +138,11 @@ void parseCommand(char* command, int* exitStatus)
 			continue;
 		}
 		args[i] = malloc(sizeof(char)*strlen(token));
-		printf("args[%d]: %s\n",i, args[i]);
-		printf("Token: %s\n", token);
 		strcpy(args[i], token);
-		printf("args[%d]: %s\n",i,args[i]);
-		printf("numArgs: %d\n", numArgs);
-		printf("line 151\n");
 		//removes new line character
 		args[i][strcspn(args[i], "\n")] = 0;
 		token = strtok(NULL, s);
 	}
-	printf("line 155\n");
 	args[numArgs-1] = NULL;
 
 	//set input and output files for background process
@@ -180,7 +159,6 @@ void parseCommand(char* command, int* exitStatus)
 			strcpy(outputFile, "/dev/null");
 		}
 	}
-	printf("line 172\n");
 
 
 	checkCommand(args, numArgs, inputFile, outputFile, exitStatus, background);
@@ -188,7 +166,6 @@ void parseCommand(char* command, int* exitStatus)
 
 void checkCommand(char** args, int numArgs, char* inputFile, char* outputFile, int* exitStatus, int background)
 {
-	printf("line 180\n");
 	if(strcmp(args[0],"cd")==0)
 	{
 		changeDirectory(args, numArgs);
@@ -210,20 +187,16 @@ void checkCommand(char** args, int numArgs, char* inputFile, char* outputFile, i
 		{
 			free(args[i]);
 		}
-		printf("line 202\n");
 		exit(0);
 	}else if(args[0][0] == '#'||strcmp(args[0],"")==0)
 	{
 		//comment line so do nothing
 	}else {
 		//exec shell
-		printf("line 209\n");
 		*exitStatus = executeShell(args, inputFile, outputFile, numArgs, background);
 	}
-	printf("line 212\n");
 	free(inputFile);
 	free(outputFile);
-	printf("line 215\n");
 }
 
 void changeDirectory(char** args, int numArgs)
@@ -249,14 +222,12 @@ void changeDirectory(char** args, int numArgs)
 		printf("%s\n", currentDir);
 		fflush(stdout);
 	}
-	printf("line 241\n");
 }
 
 int executeShell(char** args, char* inputFile, char* outputFile, int numArgs, int background)
 {
 	pid_t spawnPid = -5;
 	int childExitStatus = -5;
-	printf("line 248\n");
 	spawnPid = fork();
 	switch (spawnPid) {
 		case -1: { 
@@ -267,7 +238,6 @@ int executeShell(char** args, char* inputFile, char* outputFile, int numArgs, in
 		case 0: {
 			//necessary redirection
 			int sourceFD, targetFD, result;
-			printf("line 259\n");
 			if(inputFile!=NULL)
 			{
 				sourceFD = open(inputFile, O_RDONLY);
@@ -286,7 +256,6 @@ int executeShell(char** args, char* inputFile, char* outputFile, int numArgs, in
 				fcntl(targetFD, F_SETFD, FD_CLOEXEC);
 			}
 
-			printf("line 278\n");
 			execvp(args[0], args);
 			exit(2); 
 			break;
@@ -309,7 +278,6 @@ int executeShell(char** args, char* inputFile, char* outputFile, int numArgs, in
 
 				fgpid = INT_MAX;
 
-				printf("line 301\n");
 
 				// if process was terminated by signal, print message
 				if (signalNum != 0)
@@ -318,7 +286,6 @@ int executeShell(char** args, char* inputFile, char* outputFile, int numArgs, in
 				}
 
 			}else{
-				printf("line 310\n");
 				pid_t childPID = waitpid(childPID, &childExitStatus, WNOHANG);
 				bgpid[cur++] = childPID;
 				printf("background pid is %d\n", spawnPid);
@@ -329,22 +296,18 @@ int executeShell(char** args, char* inputFile, char* outputFile, int numArgs, in
 		}
 	}
 
-	printf("line 321\n");
 	int i=0;
 	for(i=0; i<numArgs; i++)
 	{
 		free(args[i]);
 	}
 	free(args);
-	printf("line 328\n");
-
 	return childExitStatus;
 }
 
 void fgHandler()
 {
 
-	printf("line 336\n");
 	if(fgOnly == 0)
 	{
 		printf("Entering foreground-only mode (& is now ignored)\n");
@@ -353,7 +316,6 @@ void fgHandler()
 		printf("Exiting foreground-only mode\n");
 		fgOnly = 0;
 	}
-	printf("line 345\n");
 
     return;
 }
@@ -362,7 +324,6 @@ void fgHandler()
 
 void sigintHandler()
 {
-	printf("line 354\n");
     // if interrupt signal occurs while fg process is running, kill it
     if (fgpid != INT_MAX)
     {
@@ -373,7 +334,6 @@ void sigintHandler()
         // set global variable for status messages
         signalNum = 2;  
     }  
-	printf("line 365\n");
 
     // ignore interrupt signal for all other processes
     // and simply return
@@ -382,7 +342,6 @@ void sigintHandler()
 
 int main() {
 
-	printf("line 374\n");
 	stopFg_act.sa_handler = fgHandler;
 	stopFg_act.sa_flags = SA_RESTART;
 	sigfillset(&(stopFg_act.sa_mask));
@@ -397,10 +356,8 @@ int main() {
 	restOfTheTime_act.sa_flags = SA_RESTART;
 	sigfillset(&(restOfTheTime_act.sa_mask));
 	sigaction(SIGINT, &restOfTheTime_act, NULL); 
-	printf("line 389\n");
 
 	runShell();	
-	printf("final\n");
 	return 0;
 
 }
